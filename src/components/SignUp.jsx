847057469fd696610448a09b3e4aed95a7946daf
Kelvin Cj <ckelvin196@gmail.com>
2023-10-03 22:05:08 +0100

    connected API react-toastify to display API status

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -10,6 +10,10 @@ import { useState } from "react"
 import { Link } from "react-router-dom"
 import { BsEyeSlash , BsEye } from "react-icons/bs";
 
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
 function Icon() {
   return (
     <svg
@@ -17,56 +21,75 @@ function Icon() {
       viewBox="0 0 24 24"
       fill="currentColor"
       className="h-6 w-6"
-    >
+      >
       <path
         fillRule="evenodd"
         d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
         clipRule="evenodd"
-      />
+        />
     </svg>
   );
 }
 
 function SignUp() {
-
+  
   const [firstname , setFirstname] = useState("")
   const [lastname , setLatname] = useState("")
   const [username , setUsername] = useState("")
-  const [email , setEmail] = useState("")
+  
   const [password , setPassword] = useState("")
   const [confirmPwd , setConfirmPwd] = useState("")
-  const [number , setNumber] = useState("")
+  
+  const [view, setView] = useState(false);
+  const [fail, setFail] = useState(false);
 
-   const handlesubmit = (e) => {
-    e.preventDefault()
-    console.log(firstname);
-    console.log(lastname);
-    console.log(username);
-    console.log(email);
-  if(password == confirmPwd){
-    console.log('password match !!!');
-  }else{
-    console.log("password did not match");
-    setFail(true)
-    setMatch("red")
-  }
-    console.log(number);
+   const handlesubmit = async (e) => {
+   
+    e.preventDefault();
+    if(6 > password.length){
+      console.log("Error");
+    }else{
+      if(password === confirmPwd){
+    
+        try {
+         
+          const response = await fetch("https://result-checker-g7zf.onrender.com/api/register", {
+          method: "POST",
+          mode: "cors",
+          headers : {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            firstName: firstname, 
+            lastName: lastname ,
+             password , 
+             confirm_password: confirmPwd , 
+             username
+          })
+        })
+          const data = await response.json();
+          console.log(data);
+      toast.success("Success" , {toastId  : "custom-id"})
+      window.location.href="/login"
+        } catch(err) {
+    
+    toast.error("Sign Up Failed")
+        }
+      }else{
+        setFail(true)
+      }
+    }
+  
+ 
+   
     // window.location.href = "/login"
    }
 
-// const failed = () => {
-//   return(
-   
-    
-//   </Alert>
-//   )
-// }    
+ 
    
 
-   const [view, setView] = useState(false);
    const [viewconf, setViewconf] = useState(false);
-   const [match, setMatch] = useState("black");
-   const [fail, setFail] = useState(false);
+  
   return (
     <div className="flex justify-center items-center mt-10">
       <Card color="transparent" shadow={false}>
@@ -75,12 +98,12 @@ function SignUp() {
           Enter your details to register.
         </Typography>
 
-        <form className="mt-6 mb-2 w-80 max-w-screen-lg sm:w-96" >
+        <form className="mt-6 mb-2 w-80 max-w-screen-lg sm:w-96" onSubmit={handlesubmit} >
           <div className="mb-4 flex flex-col gap-6">
             <Input size="lg" label="First Name" required onChange={(e) => setFirstname(e.target.value)}/>
             <Input size="lg" label="Last Name" required onChange={(e) => setLatname(e.target.value)}/>
             <Input size="lg" label="User Name" required onChange={(e) => setUsername(e.target.value)}/>
-            <Input size="lg" label="Email" inputMode="email" required onChange={(e) => setEmail(e.target.value)}/>
+            {/* <Input size="lg" label="Email" inputMode="email" required onChange={(e) => setEmail(e.target.value)}/> */}
             <Input type={view ? "text" : "password"} required size="lg" label="Password" icon={
                   view ? (
                     <BsEye
@@ -101,7 +124,7 @@ function SignUp() {
     icon={<Icon />}
    
   >Password does not match</Alert> : ''}
-            <Input type={viewconf ? "text" : "password"} required color={match} size="lg" label="ConfirmPassword" onChange={(e) => setConfirmPwd(e.target.value)}  icon={
+            <Input type={viewconf ? "text" : "password"} required size="lg" label="ConfirmPassword" onChange={(e) => setConfirmPwd(e.target.value)}  icon={
                   viewconf ? (
                     <BsEye
                       className="cursor-pointer"
@@ -114,12 +137,10 @@ function SignUp() {
                     />
                   )
                 }/>
-            <Input size="lg" type="tel" inputMode="tel" label="ContactNumber" required onChange={(e) => setNumber(e.target.value)}/>
-            <Input type="file" label="ProfilePicture" size="lg"/>
+           
+            <Input type="file" accept=".png , .jpg , .jpeg " label="ProfilePicture" size="lg"/>
           </div>
-        </form>
-
-        <Checkbox
+          <Checkbox
           label={
             <Typography
               variant="small"
@@ -136,10 +157,14 @@ function SignUp() {
             </Typography>
           } containerProps={{ className: "-ml-2.5" }}
         />
-        
-          <Button className="mt-6" onClick={handlesubmit} type="submit" fullWidth>
+          <Button className="mt-6"  type="submit" fullWidth>
             Register
           </Button>
+        </form>
+
+       
+        
+         
        
         <Typography color="gray" className="mt-4 text-center font-normal">
           Already have an account?{" "}
@@ -150,6 +175,8 @@ function SignUp() {
           </Link>
         </Typography>
       </Card>
+    <ToastContainer />
+
     </div>
   )
 }
