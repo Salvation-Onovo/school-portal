ee93b0c3772aac4b65d531fd9e024204111bf41f
Kelvin Cj <ckelvin196@gmail.com>
2023-10-04 07:54:14 +0100

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,18 @@
 import { SideBar } from "."
+import Table from "./Table"
 
 
 
 function Home() {
   return (
     <div className="flex flex-col">
-
       <div className="flex justify-between items-center mx-3">
       <SideBar/>
-      !3th September , 2023
+      13th September , 2023
       </div>
+      <main>
+      <Table/>
+      </main>
     </div>
   )
 }
